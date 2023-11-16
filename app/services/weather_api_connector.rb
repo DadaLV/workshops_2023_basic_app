4c135078620e3df90cf06944728bc8e72d7c3dc1
DadaLV <vynohradova.l.a@gmail.com>
2023-11-16 13:19:29 +0100

    Fix syntax issues

--- app/services/weather_api_connector.rb
+++ app/services/weather_api_connector.rb
@@ -1,12 +1,12 @@
 require 'net/http'
 class WeatherApiConnector
   API_KEY = A9n.weather_api_key
-  LOCATION = 'Cracow'
+  LOCATION = 'Cracow'.freeze
 
   def weather_data
     url = "http://api.weatherapi.com/v1/current.json?key=#{API_KEY}&q=#{LOCATION}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     JSON.parse(response)
-  end  
+  end
 end
